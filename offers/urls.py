07bc09b3cf3c0a9d20c1bbfc5fea1d94584a8a18
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2024-03-14 19:03:05 +0100

    Add basic templates - about and contact which fixes: #6

--- offers/urls.py
+++ offers/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="view-home"),
     path('offers', views.offers, name="view-offers"),
+    path('about', views.about, name="view-about"),
+    path('contact', views.contact, name="view-contact"),
 ]
--- offers/views.py
+++ offers/views.py
@@ -5,3 +5,9 @@ def home(request):
 
 def offers(request):
     return render(request, template_name="offers.html")
+
+def about(request):
+    return render(request, template_name="about.html")
+
+def contact(request):
+    return render(request, template_name="contact.html")
